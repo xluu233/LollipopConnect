c268e621cd4c77c31e0956d3860377c4afe8fc15
bugmaker <luhongchen@foamxil.com>
2023-12-17 21:38:19 +0800

    init
    init Clion and QtQuick config

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+//    const QUrl url(QStringLiteral("qrc:/player/qml/main.qml"));
+    const QUrl url(u"qrc:/player/qml/main.qml"_qs);
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                if (!obj && url == objUrl)
+                    QCoreApplication::exit(-1);
+            }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
